a229084b4154f97a0298b7bade8339bc6c6f0dc7
Mutethia Gatobu <evansgaji@gmail.com>
2017-03-19 17:43:59 -0600

    no-server

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
-TweenMax.to(".ball", 4.0, {
-	ease: Bounce.easeOut,
-	y: 295,
-	x:10,
-	rotation: 360
+angular.module('myApp',['ui.router'])
+	.config(function($stateProvider, $urlRouterProvider, $sceDelegateProvider){
+		$stateProvider
+		.state ('home', {
+			url:'/',
+			templateUrl:"../view/home.html",
+			controller: 'homeCtrl'
+		})
+		.state ('info', {
+			url:'/info',
+			templateUrl:"../view/info.html",
+			controller: 'infoCtrl'
+
+		})
+		.state ('contact', {
+			url: '/contact',
+			templateUrl:"../view/contact.html"
+
+		});
+
+	$sceDelegateProvider.resourceUrlWhitelist([
+   // Allow same origin resource loads.
+   'self',
+   // Allow loading from our assets domain.  Notice the difference between * and **.
+   'http://www.hqcasanova.com/**']);
+
 });
-TweenMax.to(".ball", 4.5, {
-	x:900,
-	rotation: 360,
-	delay: 0,
-})
+
+// TweenMax.to(".ball", 4.0, {
+// 	ease: Bounce.easeOut,
+// 	y: 295,
+// 	x:10,
+// 	rotation: 360
+// });
+// TweenMax.to(".ball", 4.5, {
+// 	x:900,
+// 	rotation: 360,
+// 	delay: 0,
+// })
